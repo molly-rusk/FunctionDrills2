91309787b3904963f90c84ce0b8e077a7bbe1823
Molly Rusk <mollyrusk@gmail.com>
2021-11-17 16:26:47 -0700

    Problem 1 complete

--- arrayMethods.js
+++ arrayMethods.js
@@ -17,9 +17,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-// CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(element => element % 2 === 0)
 
+console.log(evenNumbers)
 
 
 ////////// PROBLEM 2 //////////
@@ -30,17 +30,16 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 
 /*
   Use the map method on the prices array to calculate a new array of post-tax prices.
-  Use a 7% tax rate.
-  Math reminder! To calculate the price after tax, multiply the price by 1 plus the taxRate as a decimal.
-  Example: if tax is 7%, the price afterTax could be calculated like this: afterTax = price * 1.07) 
-  The map function also takes a callback with the parameters
-  function(element, index, wholeArray){}  Function Form
+  Use a 7% tax rate. Math reminder! To calculate the price after tax, multiply the price 
+  by 1 plus the taxRate as a decimal. Example: if tax is 7%, the price afterTax could be 
+  calculated like this: afterTax = price * 1.07) The map function also takes a callback with
+   the parameters function(element, index, wholeArray){}  Function Form
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-// CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
 
+
+const postTaxPrices = prices.map(
 
 
 ////////// PROBLEM 3 //////////
@@ -57,7 +56,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+//const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
@@ -82,7 +81,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+//const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
 
 
